abd5ecc4962ffa41e255fee52f2774711c5ae26b
westy12dan <westy12dan@gmail.com>
2022-05-28 14:56:56 +0800

    Add question

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
-import { Flex, VStack } from '@chakra-ui/react'
+import { Flex, VStack, Text } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import TokenParagraph from '../components/TokenParagraph'
 import { useStore } from '../store'
 import Health from '../components/Health'
+import { plural } from '../utils/plural'
 
 useStore
     .getState()
@@ -13,12 +14,16 @@ useStore
     })
 
 const Home: NextPage = () => {
+    const answers = useStore((s) =>
+        Object.values(s.tokenMap).filter((s) => s.answer)
+    )
     return (
         <Flex
             minH="100vh"
             minW="100vw"
             alignItems="center"
             justifyContent="center"
+            p={6}
         >
             <Head>
                 <title>Classroom Games</title>
@@ -29,7 +34,19 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <VStack spacing={16}>
-                <TokenParagraph />
+                <VStack spacing={12}>
+                    <Text color="gray.500" align="center">
+                        There{' '}
+                        {plural(
+                            answers.length,
+                            `is ${answers.length} error`,
+                            `are ${answers.length} errors`
+                        )}{' '}
+                        in this paragraph, can you find{' '}
+                        {plural(answers.length, 'it', 'them all')}?
+                    </Text>
+                    <TokenParagraph />
+                </VStack>
                 <Health />
             </VStack>
         </Flex>
