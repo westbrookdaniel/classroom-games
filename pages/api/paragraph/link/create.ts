61b0e76ef13e4ff21e0ba436e4d57af2b301e458
westy12dan <westy12dan@gmail.com>
2022-06-18 14:36:41 +0800

    Update error messages

--- pages/api/paragraph/link/create.ts
+++ pages/api/paragraph/link/create.ts
@@ -12,28 +12,28 @@ export default async function handler(
     res: NextApiResponse<ResponseData>
 ) {
     if (req.method !== 'POST')
-        return res.status(405).send({ error: 'method not allowed' })
+        return res.status(405).send({ error: 'Method not allowed' })
 
     const { code, paragraph, hideCreate } = JSON.parse(req.body)
 
     if (typeof paragraph !== 'string')
-        return res.status(400).send({ error: 'paragraph is not valid' })
+        return res.status(400).send({ error: 'Paragraph is not valid' })
 
     if (typeof code !== 'string')
-        return res.status(400).send({ error: 'code is not valid' })
+        return res.status(400).send({ error: 'Code is not valid' })
 
     if (code === 'create')
         return res
             .status(400)
-            .send({ error: 'create can not be used as a code' })
+            .send({ error: 'Create can not be used as a code' })
 
     if (typeof hideCreate !== 'boolean')
-        return res.status(400).send({ error: 'hideCreate is not valid' })
+        return res.status(400).send({ error: 'Something went wrong, please try refreshing' })
 
     if (await getLinkByCode({ code }))
         return res
             .status(400)
-            .send({ error: 'link with this code already exists' })
+            .send({ error: 'A link with this code already exists' })
 
     const gameLink = await createLink({ code, paragraph, hideCreate })
     res.status(200).json({ data: gameLink })
